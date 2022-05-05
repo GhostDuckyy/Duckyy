b0af8a51343c10e0fb473b24faa87df1ec8fa314
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2022-05-05 13:00:37 +0800

    Add files via upload

---
+++ Loader.lua
@@ -0,0 +1,14 @@
+local list = {
+    a4225025295 = "Op_Ninja_Simulator.lua",
+    a7560156054 = "Clicker_Simulator.lua",
+    a2619187362 = "Super_Power_Fighting_Simulator.lua",
+}
+local repo = "https://raw.githubusercontent.com/GhostDuckyy/Duckyy/main/"
+local load = loadstring
+for index, value in pairs(list) do
+    if "a"..game.PlaceId == index then
+        pcall(function()
+            load(game:HttpGet(repo..value, true))()
+        end)
+    end
+end
