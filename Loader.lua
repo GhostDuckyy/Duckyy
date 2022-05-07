0343881cb38eab6bcc9e134c47464e8f1aa17693
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2022-05-07 22:22:48 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -2,6 +2,7 @@ local list = {
     a4225025295 = "Op_Ninja_Simulator.lua",
     a7560156054 = "Clicker_Simulator.lua",
     a2619187362 = "Super_Power_Fighting_Simulator.lua",
+    a8540346411 = "Rebirth_Champions_X.lua"
 }
 local repo = "https://raw.githubusercontent.com/GhostDuckyy/Duckyy/main/"
 local load = loadstring
